95bee5d76846e2d5cc5d0f7f92bcedc6ef36ad30
Robert Seedorf <seedor87@students.rowan.edu>
2017-02-12 20:35:14 -0500

    created set generator for topics. TODO work on a way to pull out verbs for topics

--- Parser/Topic.py
+++ Parser/Topic.py
@@ -10,5 +10,8 @@ class Topic(object):
     def __eq__(self, other):
         return self.get_text() == other.get_text()
 
+    def __str__(self):
+        return self.text
+
     def get_text(self):
         return self.text
