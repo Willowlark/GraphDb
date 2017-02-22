8310dd2968def1897157665b66fc6c6cdd9e5675
Robert Seedorf <seedor87@students.rowan.edu>
2017-02-21 22:52:19 -0500

    Final commit to branch parser. Added comments and touch ups on code and comment formatting. ready to push

--- Parsing/Parser.py
+++ Parsing/Parser.py
@@ -3,8 +3,8 @@ from __future__ import division
 import nltk, sys, validators, os
 from pprint import pprint
 from collections import defaultdict
-# from Topic_Candidate import Topic_Candidate
-from Topic import Topic_Candidate
+from Topic_Candidate import Topic_Candidate
+# from Topic import Topic_Candidate
 
 """
 This file stores the static methods to interpret topic candidates from zero or more bodies of text.
@@ -20,10 +20,10 @@ def structured_topic(*kargs):
 def parse_topics(*kargs):
     """
     Used for parsing topic candidates, form conjunctive crucial words, that are of the Noun persuasion.
-    These instances of topic candidate that are returned will be used in the database as topics for relation creation
+    The instances of topic candidate that are returned will be used in the database as units for relation creation
 
-    `kargs` - the params (in the form zero or more) raw bodies of text to be processed
-    `yield` topic candidates - the generated collection of topics that have been extracted and constructed
+    `kargs` - the params (in the form zero or more) of raw bodies of text to be processed
+    `yield` topic candidates - the generated collection of Noun based topics that have been extracted and constructed
     """
     for body_of_text in kargs:
         processed = info_extract_preprocess(body_of_text)   # preprocessed body for tagged words in sentence form
@@ -35,10 +35,12 @@ def parse_topics(*kargs):
 
 def parse_topics_not_nouns(*kargs):
     """
-    Method used for parsing topics that are not conjunctive noun words
+    Method used for parsing topics that are not conjunctive noun words.
+    Second to the NP form that is preferred use. Does not produce effective NNPs.
+    However, only means to yield the non-nouns words of interest
 
     `kargs` - the params (in the form zero or more) raw bodies of text to be processed
-    `yield` topic candidates - the generated collection of topics that have been extracted and constructed
+    `yield` topic candidates - the generated collection of non-Noun topics that have been extracted and constructed
     """
     for body_of_text in kargs:
         processed = info_extract_preprocess(body_of_text)   # preprocessed body for tagged words in sentence form
@@ -54,10 +56,18 @@ def is_structured(data):
 
 
 def info_extract_preprocess(document):
-   sentences = nltk.sent_tokenize(document) #tokenize sentence into sentences
-   tokenized = [nltk.word_tokenize(sent) for sent in sentences] # tokenize sentences in sentences
-   tagged = [nltk.pos_tag(sent) for sent in tokenized] # tag the sentences in tokenized
-   return tagged
+    """
+    This method is used to perform the reiterative process of preparing the given body of text to be parsed.
+    First the sentences are tokenized using the default method of sent_tokenize(), next, the words are tokenized one by one.
+    Finally those words are tagged by the default tagger of pos_tagger()
+
+    `document` the string form body of text to be pre-processed
+    `return` tagged - the tokenized, then tagged body of sentence delimited text.
+    """
+    sentences = nltk.sent_tokenize(document) #tokenize sentence into sentences
+    tokenized = [nltk.word_tokenize(sent) for sent in sentences] # tokenize sentences in sentences
+    tagged = [nltk.pos_tag(sent) for sent in tokenized] # tag the sentences in tokenized
+    return tagged
 
 def _get_continuous_chunks_NP(tagged):
     """
@@ -87,8 +97,10 @@ def _get_continuous_chunks_NP(tagged):
 
 def _get_non_NP_chunks(tagged):
     """
+    This method is the only viable alternative to gathering stats on non-noun base words for topic making.
+    Leverages the concept of grammar parsing (or using a commented out regex parser) to garner verb and adjective topics
 
-    `tagged`
+    `tagged` - the pre-processed, sentence delimited, tokenized, tagged, body of text
     `return` labels, counts - the affiliated labels and count of appearances for likely topics
     """
     labels = defaultdict(set) # the dictionary of labels whose values are associated sets of topics
@@ -105,15 +117,20 @@ def _get_non_NP_chunks(tagged):
     """
     An alternative to pre-def grammar features chunking and chinking rules.
     This method failed to provide an increase in accuracy but will remain for posterity.
+
+    >>> chunk_rule = ChunkRule("<.*>+", "Chunk everything")
+    >>> chink_rule = ChinkRule("<VB\.>", "Chink on verbs/prepositions")
+    >>> split_rule = SplitRule("<NN><VB>", "<DT><NN>",
+    >>> "Split successive determiner/noun pairs")
+    >>> chunk_parser = RegexpChunkParser([chunk_rule, chink_rule, split_rule], chunk_label = 'VB')
     """
-    # chunk_rule = ChunkRule("<.*>+", "Chunk everything")
-    # chink_rule = ChinkRule("<VB\.>", "Chink on verbs/prepositions")
-    # split_rule = SplitRule("<NN><VB>", "<DT><NN>",
-    # "Split successive determiner/noun pairs")
-    # chunk_parser = RegexpChunkParser([chunk_rule, chink_rule, split_rule], chunk_label = 'VB')
 
     for sentence in tagged:
-        # chunked = chunk_parser.parse(sentence)
+        """
+        line used in tandem with the alt chunker seen above
+
+        >>> chunked = chunk_parser.parse(sentence)
+        """
         chunked = cp.parse(sentence)
         for i in chunked:
             if type(i) != nltk.Tree and (i[1].startswith('V') or i[1].startswith('J')):
@@ -122,6 +139,9 @@ def _get_non_NP_chunks(tagged):
     return labels, counts
 
 def _process_input(input):
+    """
+    used by main and debugger for testing purposes
+    """
 
     def process_string(string):
         text = string.strip()
@@ -182,21 +202,17 @@ if __name__ == '__main__':
 """
 Used only for investigation of lemmatization.
 Remnants kept in case it can be found useful.
+
+def lemmatize_investigate():
+    body = _process_input('http://www.foxnews.com/politics/2017/02/08/white-house-fires-back-at-immigration-order-critics-with-list-terror-arrests.html')
+    tokenized = nltk.word_tokenize(body)
+    tagged = nltk.pos_tag(tokenized)
+    gen = generate_frequency_by_pos(tagged=tagged, pos=('JJ', 'VB'))
+    from nltk.stem import WordNetLemmatizer
+    wordnet_lemmatizer = WordNetLemmatizer()
+    for elem in gen:
+        print elem[0],
+        for title, strength in elem[1]:
+            print title, wordnet_lemmatizer.lemmatize(title), ',',
+        print
 """
-# def lemmatize_investigate():
-#     body = _process_input('http://www.foxnews.com/politics/2017/02/08/white-house-fires-back-at-immigration-order-critics-with-list-terror-arrests.html')
-#     tokenized = nltk.word_tokenize(body)
-#     tagged = nltk.pos_tag(tokenized)
-#     gen = generate_frequency_by_pos(tagged=tagged, pos=('JJ', 'VB'))
-#     from nltk.stem import WordNetLemmatizer
-#     wordnet_lemmatizer = WordNetLemmatizer()
-#     for elem in gen:
-#         print elem[0],
-#         for title, strength in elem[1]:
-#             print title, wordnet_lemmatizer.lemmatize(title), ',',
-#         print
-
-
-
-
-
