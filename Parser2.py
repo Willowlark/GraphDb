1971d9f649cdf5ed139c535cb792dd8f60a64660
Robert Seedorf <seedor87@students.rowan.edu>
2017-03-27 16:49:28 -0400

    committing very clean work on the new parser

--- Parser2.py
+++ Parser2.py
@@ -76,7 +76,7 @@ class Topic_Candidate(object):
     def __str__(self):
         return str(self.title)
 
-    def __init__(self, topic, strength, label, after, before, suffix, prefix, depth):
+    def __init__(self, topic, strength=None, label=None, after=None, before=None, suffix=None, prefix=None, depth=None):
         self.title = topic
         self.strength = strength
         self.label = label
@@ -85,6 +85,18 @@ class Topic_Candidate(object):
         self.depth = depth
         self.suffix = suffix
         self.prefix = prefix
+
+    def __eq__(self, other):
+        return self.title == other.title
+
+    def __ne__(self, other):
+        return self.title != other.title
+
+    def __hash__(self):
+        return hash(self.title)
+
+    def append_after(self, word):
+        self.after.append(word)
 
 # document = """Write a page that shows some data.
 # I don't particularly care what it is, but there should be enough to be interesting.
@@ -101,58 +113,58 @@ class Topic_Candidate(object):
 # where Albus is your Elvis username.
 # ENJOY!"""
 
-document = """ASRC Federal Mission Solutions Rowan Collaboration Program – Fall, 2016
+# document = """ASRC Federal Mission Solutions Rowan Collaboration Program – Fall, 2016
+#
+# Graph Database Evaluation and Reference Implementation
+# Executive Summary
+# A graph database is a database that uses graph structures for queries with nodes, relationships and properties to represent and store data.   Within graph databases, the relationship (and its subsequent properties), is just as important as the node itself.   This relationship allows data in each node to be directly coupled in a unique way, decreasing the time and complexity required to gather associated pieces of information that are related to the object in question.  The relationships contain their own metadata, which is used as part of the query process, providing another layer of filtering for the data queries and results.
+# It is with these relationships that graph databases differ from conventional relational databases.  Relational databases store their relationship links in the data itself.  Queries searching for associated data need to traverse the data store and use the JOIN concept to collect the related data.  Depending on the size of the information and the number of associations one needs to manage, relational database implementations can lead to complex and time consuming queries.  Maintaining such a store as associations come and go can also be a tedious task within a relational database.  Graph databases, by design, allow simple and rapid retrieval of complex hierarchical structures that are difficult to model in relational systems.
+# Background
+# The increased power, capabilities and adoption rates of computers and communications systems are generating exponentially increasing amounts of data.  Within the C4 intelligence[a] pworld that data needs to analyzed for significant events, data or patterns to counter threats, stop the spread of disease and provide resource support to name a few.  In most cases information is collected and managed in stovepipes.  That information is then analyzed by experts within that stovepipe and decisions are made based on that data.  Unfortunately, by ponly using one stream of information, pieces of information may be missing from the overall decision process, especially if that missing information is from a totally different domain altogether.  For example, an analyst reviewing satellite data may be missing weather service and social media information that could better round out the decision process.  In many cases an analyst may have access to multiple feeds of information and will have to establish relationships between that information such as time and location to provide a more rounded out view of the item of interest.
+# This leads us to why we want to explore Graph databases.  Imagine an analyst trying to manage information based on a multitude of people, places and things while also trying to establish and maintain information relationships to those objects at the same time within a relational database.[b]  The tables and queries would constantly be changing to accommodate new information (both existing and new formats) coming and going.  The timeliness [c]of retrieving the necessary information from such data store, that could be distributed, would
+# become problematic over time.  Another issue would be completeness [d]of the data as knowing whether all the associations are accounted for would be difficult to determine.  Graph databases appear to solve these issues by decoupling the data from the relationship and in turn aligning properties with the relationships so the information and its metadata can be view and queried based on the objects of interest solely.
+# Tasking
+# As part of this effort we would like the team to focus on two areas:  An evaluation of prominent graph databases to determine a viable approach to meet the needs of the above scenarios and a reference implementation that demonstrates the strength of such a database in a domain with multiple disparate data feeds.[e]  A breakdown of the task is as follows:
+# * Evaluate a few prominent  Graph DB implementations (Neo4j is a popular one) and identify/document pros/cons based on the following criteria:
+#    * Performance - how the database under varying conditions
+#    * Organization – how is the information retained, is this different between products
+#    * Scaling – How do the products handle scaling up data content and complexity of queries
+#    * Cost – What is the cost/licensing approach for each product.  Which are free…but do they have a license constraint.
+#    *  Robustness of query language – How flexible is the language to perform simple to complex queries and is it easy to understand.
+#    * Utilization – Who’s using it and on what products?
+#    * Support – Is the product maintained, how often are updates produced?
+#    * Product configurations - can the product support a web/cloud solution, standalone, networked, clusters?
+#    * Services  - backup, clustering support, security, etc
+#    * Visualization solutions – Can we easily visualize the data via a plugin or third party solution or do we have to implement our own solution? (e.g. Keyline looked like they had some solutions)
+#    * Notification of changes – How does one get notified by changes to the data pfrom the database product.
+#    * Batch updates – Does the product support a batch update model or is a single transaction at a time?
+#    *  Data interoperability – What level of data conditioning needs to happen before it can be stored in the database?  For example, relational databases require the user to populate a defined schema layout; ORM is based on a predefined annotated class.  Is it name value pairs, etc?  How flexible is it to handling new data types/formats?
+#    * Space utilization – Does the product use some kind of data compression?
+#    * Transaction support – Does the product support this?
+#    * “Time machine analysis” - The ability to look back in time at the data and potentially update it to see how actionable data has changed.
+# * Pick one and define an approach to utilizing the database in both local and web/cloud based deployment environments.
+# * Build reference implementations of a graph database supporting a C4 intelligence based problem.  Utilize disparate data points to build relationships that can be used to build actionable activity detection[f].  Possibly use data from resources such as data.gov as feeds.  Do we have any other data we can point them to?[g]
+#    * Establish approaches for identifying/establishing relationships between the various disparate data feeds (e.g. time, location, name, etc).
+#
+#
+# All developed artifacts are to be stored on Github.
+# Goals
+# * Evaluate a few Graph DB solutions and document in a white paper comparative details and how each DB performed against the above criteria.
+# * A selected “best of breed” graph database to meet our C4 Intelligence needs (multiple disparate data feeds, some with significant volume).
+# * A reference implementation of that database against a C4 Intelligence based scenario that showcases the value of graph databases within that community for the management of disparate data as well as the efficient deduction of action information, due to the relationships, of that data.
+# * Recommendations for how to identify and build relationships from the data feeds.
+#
+#
+# [a]Command Control Center and What?
+# [b]What we will assume is an inevitable goal of someone in this work
+# [c]point of interest 1
+# [d]point of interest 2
+# [e]Two strongest points of writings to be sought
+# [f]good name, great objective
+# [g]What does other data constitute? other software that can be used, or maybe other information on the work so far? What does the future hold?"""
 
-Graph Database Evaluation and Reference Implementation
-Executive Summary
-A graph database is a database that uses graph structures for queries with nodes, relationships and properties to represent and store data.   Within graph databases, the relationship (and its subsequent properties), is just as important as the node itself.   This relationship allows data in each node to be directly coupled in a unique way, decreasing the time and complexity required to gather associated pieces of information that are related to the object in question.  The relationships contain their own metadata, which is used as part of the query process, providing another layer of filtering for the data queries and results.
-It is with these relationships that graph databases differ from conventional relational databases.  Relational databases store their relationship links in the data itself.  Queries searching for associated data need to traverse the data store and use the JOIN concept to collect the related data.  Depending on the size of the information and the number of associations one needs to manage, relational database implementations can lead to complex and time consuming queries.  Maintaining such a store as associations come and go can also be a tedious task within a relational database.  Graph databases, by design, allow simple and rapid retrieval of complex hierarchical structures that are difficult to model in relational systems.
-Background
-The increased power, capabilities and adoption rates of computers and communications systems are generating exponentially increasing amounts of data.  Within the C4 intelligence[a] pworld that data needs to analyzed for significant events, data or patterns to counter threats, stop the spread of disease and provide resource support to name a few.  In most cases information is collected and managed in stovepipes.  That information is then analyzed by experts within that stovepipe and decisions are made based on that data.  Unfortunately, by ponly using one stream of information, pieces of information may be missing from the overall decision process, especially if that missing information is from a totally different domain altogether.  For example, an analyst reviewing satellite data may be missing weather service and social media information that could better round out the decision process.  In many cases an analyst may have access to multiple feeds of information and will have to establish relationships between that information such as time and location to provide a more rounded out view of the item of interest.
-This leads us to why we want to explore Graph databases.  Imagine an analyst trying to manage information based on a multitude of people, places and things while also trying to establish and maintain information relationships to those objects at the same time within a relational database.[b]  The tables and queries would constantly be changing to accommodate new information (both existing and new formats) coming and going.  The timeliness [c]of retrieving the necessary information from such data store, that could be distributed, would
-become problematic over time.  Another issue would be completeness [d]of the data as knowing whether all the associations are accounted for would be difficult to determine.  Graph databases appear to solve these issues by decoupling the data from the relationship and in turn aligning properties with the relationships so the information and its metadata can be view and queried based on the objects of interest solely.
-Tasking
-As part of this effort we would like the team to focus on two areas:  An evaluation of prominent graph databases to determine a viable approach to meet the needs of the above scenarios and a reference implementation that demonstrates the strength of such a database in a domain with multiple disparate data feeds.[e]  A breakdown of the task is as follows:
-* Evaluate a few prominent  Graph DB implementations (Neo4j is a popular one) and identify/document pros/cons based on the following criteria:
-   * Performance - how the database under varying conditions
-   * Organization – how is the information retained, is this different between products
-   * Scaling – How do the products handle scaling up data content and complexity of queries
-   * Cost – What is the cost/licensing approach for each product.  Which are free…but do they have a license constraint.
-   *  Robustness of query language – How flexible is the language to perform simple to complex queries and is it easy to understand.
-   * Utilization – Who’s using it and on what products?
-   * Support – Is the product maintained, how often are updates produced?
-   * Product configurations - can the product support a web/cloud solution, standalone, networked, clusters?
-   * Services  - backup, clustering support, security, etc
-   * Visualization solutions – Can we easily visualize the data via a plugin or third party solution or do we have to implement our own solution? (e.g. Keyline looked like they had some solutions)
-   * Notification of changes – How does one get notified by changes to the data pfrom the database product.
-   * Batch updates – Does the product support a batch update model or is a single transaction at a time?
-   *  Data interoperability – What level of data conditioning needs to happen before it can be stored in the database?  For example, relational databases require the user to populate a defined schema layout; ORM is based on a predefined annotated class.  Is it name value pairs, etc?  How flexible is it to handling new data types/formats?
-   * Space utilization – Does the product use some kind of data compression?
-   * Transaction support – Does the product support this?
-   * “Time machine analysis” - The ability to look back in time at the data and potentially update it to see how actionable data has changed.
-* Pick one and define an approach to utilizing the database in both local and web/cloud based deployment environments.
-* Build reference implementations of a graph database supporting a C4 intelligence based problem.  Utilize disparate data points to build relationships that can be used to build actionable activity detection[f].  Possibly use data from resources such as data.gov as feeds.  Do we have any other data we can point them to?[g]
-   * Establish approaches for identifying/establishing relationships between the various disparate data feeds (e.g. time, location, name, etc).
-
-
-All developed artifacts are to be stored on Github.
-Goals
-* Evaluate a few Graph DB solutions and document in a white paper comparative details and how each DB performed against the above criteria.
-* A selected “best of breed” graph database to meet our C4 Intelligence needs (multiple disparate data feeds, some with significant volume).
-* A reference implementation of that database against a C4 Intelligence based scenario that showcases the value of graph databases within that community for the management of disparate data as well as the efficient deduction of action information, due to the relationships, of that data.
-* Recommendations for how to identify and build relationships from the data feeds.
-
-
-[a]Command Control Center and What?
-[b]What we will assume is an inevitable goal of someone in this work
-[c]point of interest 1
-[d]point of interest 2
-[e]Two strongest points of writings to be sought
-[f]good name, great objective
-[g]What does other data constitute? other software that can be used, or maybe other information on the work so far? What does the future hold?"""
-
-# document = body =_process_input(
-#         'http://www.foxnews.com/us/2017/02/10/marine-vet-speaks-out-about-viral-video-supporting-trump-travel-ban.html')
+document = body =_process_input(
+        'http://www.foxnews.com/us/2017/02/10/marine-vet-speaks-out-about-viral-video-supporting-trump-travel-ban.html')
 
 
 def preproc(document):
@@ -219,6 +231,53 @@ def _new_get_continuous_chunks_NP(tagged):
 
     return dict(labels), dict(counts), dict(befores), dict(afters), dict(suffixes), dict(prefixes), dict(depths), dict(commons)
 
+@timer
+def main2(n=None):
+    listing = []
+    tagged = preproc(document)
+    counts = defaultdict(int)
+    depth = 0
+    topic = None
+    prev_topic = None
+    before = []
+    for chunked in nltk.ne_chunk_sents(tagged, binary=False):
+        for word_tag in chunked:
+            depth += 1
+            if type(word_tag) == nltk.Tree:
+                title = " ".join([title for title, pos in word_tag.leaves()]) # the key word to be used as the likely topic
+                topic = Topic_Candidate(title, depth=depth, prefix=prev_topic, before=before, after=[])
+                before = []
+                listing.append(topic)
+                counts[title] += 1
+                if prev_topic is not None:
+                    prev_topic.suffix = topic
+                prev_topic = topic
+            else:
+                word, pos = word_tag
+                counts[word] += 1
+                before.append(word)
+                if topic is not None:
+                    topic.append_after(word)
+
+    # Build Counts Values for titles (and words) to be applied to fields of topic
+    for title, count in counts.iteritems():
+        for topic in listing:
+            if title == topic.title:
+                topic.count = count
+
+    for topic in sorted(listing, key = lambda k: k.depth): # sort by depth into the doc
+        print green(repr(topic))
+        for var in vars(topic):
+            print '\t', var, ":", getattr(topic, var)
+        print '\t', ' '.join(topic.before[-n:]), red(repr(topic)), ' '.join(topic.after[n:])
+
+    # reconstruct the context of all topics from return listing alone
+    first_topic = listing[0]
+    topic = first_topic
+    print blue(' '.join(topic.before)),
+    while topic is not None:
+        print red(str(topic)), blue(' '.join(topic.after)),
+        topic = topic.suffix
 
 @timer
 def main(n = None):
@@ -259,8 +318,8 @@ def main(n = None):
                 del w
                 break
 
-    print 'Other common words', sorted(commons.items(), key=operator.itemgetter(1))[-20:]
+    print '\nOther common words', sorted(commons.items(), key=operator.itemgetter(1))[-20:]
 
 
 if __name__ == '__main__':
-    main(10)
+    main2(10)
