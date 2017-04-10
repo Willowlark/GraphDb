2a3fcf3380d4533e4c343b9f0790e73955fb9de2
William Clark <willowlark@outlook.com>
2017-04-10 13:34:22 -0400

    __init__ filled out and html docs synchronized.

--- rss2neo/__init__.py
+++ rss2neo/__init__.py
@@ -1,4 +1,15 @@
 from os.path import dirname, basename, isfile
 import glob
+
+import sys
+from os import getcwd
+sys.path.append(getcwd())
+
+import RssGrapher
+import Feeder
+import Parser
+import Recorder
+
+
 modules = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
